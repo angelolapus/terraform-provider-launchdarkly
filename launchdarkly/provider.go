002b5c14d7a570d9fb7e695a3ce6a2d0923357f0
Lapus <angelo.paulo.a.lapus@accenture.com>
2023-11-02 18:36:33 +0800

    remove other evaluation

--- launchdarkly/provider.go
+++ launchdarkly/provider.go
@@ -113,10 +113,6 @@ func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceTyp
 
 func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
 	return map[string]tfsdk.DataSourceType{
-		"ldflags_evaluation_boolean": dataSourceFlagEvaluationBooleanType{},
-		"ldflags_evaluation_string":  dataSourceFlagEvaluationStringType{},
-		"ldflags_evaluation_int":     dataSourceFlagEvaluationIntType{},
-		"ldflags_evaluation_float":   dataSourceFlagEvaluationFloatType{},
-		"ldflags_evaluation_json":    dataSourceFlagEvaluationJSONType{},
+		"ldflags_evaluation_boolean": dataSourceFlagEvaluationBooleanType{}
 	}, nil
 }
