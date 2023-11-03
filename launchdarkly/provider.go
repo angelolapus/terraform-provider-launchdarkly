1d25aee4a43a24979e6c46b86bd57230380dddef
Lapus <angelo.paulo.a.lapus@accenture.com>
2023-11-03 10:59:57 +0800

    revert original code

--- launchdarkly/provider.go
+++ launchdarkly/provider.go
@@ -1,122 +1,151 @@
-package launchdarkly_flag_eval
+package launchdarkly
 
 import (
 	"context"
+	"net/url"
 	"os"
-	"time"
+	"strings"
 
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	ld "gopkg.in/launchdarkly/go-server-sdk.v5"
-	"gopkg.in/launchdarkly/go-server-sdk.v5/ldcomponents"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 const (
-	LAUNCHDARKLY_SDK_KEY = "LAUNCHDARKLY_SDK_KEY"
-	sdk_key              = "sdk_key"
-	host                 = "host"
-	flagKey              = "flag_key"
-	userContext          = "context"
-	variation            = "variation_type"
-	value                = "value"
-	defaultValue         = "default_value"
+	DEFAULT_LAUNCHDARKLY_HOST = "https://app.launchdarkly.com"
+	DEFAULT_HTTP_TIMEOUT_S    = 20
 )
 
-func New() tfsdk.Provider {
-	return &provider{}
-}
+// Environment Variables
+const (
+	LAUNCHDARKLY_ACCESS_TOKEN = "LAUNCHDARKLY_ACCESS_TOKEN"
+	LAUNCHDARKLY_API_HOST     = "LAUNCHDARKLY_API_HOST"
+	LAUNCHDARKLY_OAUTH_TOKEN  = "LAUNCHDARKLY_OAUTH_TOKEN"
+)
 
-type provider struct {
-	configured bool
-	client     *ld.LDClient
-}
+// Provider keys
+const (
+	ACCESS_TOKEN = "access_token"
+	OAUTH_TOKEN  = "oauth_token"
+	API_HOST     = "api_host"
+	HTTP_TIMEOUT = "http_timeout"
+)
 
-func (p *provider) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
-	return tfsdk.Schema{
-		Attributes: map[string]tfsdk.Attribute{
-			sdk_key: {
-				Type: types.StringType,
-				// must be optional to allow setting via env variable
-				Optional:    true,
-				Sensitive:   true,
-				Description: "The LaunchDarkly SDK key associated with the project and environment you would like to evaluate flags on",
-			},
-			host: {
-				Type: types.StringType,
-				// must be optional to allow setting via env variable
-				Optional:    true,
-				Sensitive:   true,
-				Description: "The host to use for performing flag evaluations.",
-			},
-			// TODO add attributes:
-			// polling/streaming
-			// other things?
+func providerSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		ACCESS_TOKEN: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The [personal access token](https://docs.launchdarkly.com/home/account-security/api-access-tokens#personal-tokens) or [service token](https://docs.launchdarkly.com/home/account-security/api-access-tokens#service-tokens) used to authenticate with LaunchDarkly. You can also set this with the `LAUNCHDARKLY_ACCESS_TOKEN` environment variable. You must provide either `access_token` or `oauth_token`.",
 		},
-	}, nil
+		OAUTH_TOKEN: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "An OAuth V2 token you use to authenticate with LaunchDarkly. You can also set this with the `LAUNCHDARKLY_OAUTH_TOKEN` environment variable. You must provide either `access_token` or `oauth_token`.",
+		},
+		API_HOST: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The LaunchDarkly host address. If this argument is not specified, the default host address is `https://app.launchdarkly.com`",
+		},
+		HTTP_TIMEOUT: {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			Description: "The HTTP timeout (in seconds) when making API calls to LaunchDarkly.",
+		},
+	}
 }
 
-type providerData struct {
-	SDKKey types.String `tfsdk:"sdk_key"`
-	Host   types.String `tfsdk:"host"`
+// Provider returns a *schema.Provider.
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: providerSchema(),
+		ResourcesMap: map[string]*schema.Resource{
+			"launchdarkly_team":                      resourceTeam(),
+			"launchdarkly_project":                   resourceProject(),
+			"launchdarkly_environment":               resourceEnvironment(),
+			"launchdarkly_feature_flag":              resourceFeatureFlag(),
+			"launchdarkly_webhook":                   resourceWebhook(),
+			"launchdarkly_custom_role":               resourceCustomRole(),
+			"launchdarkly_segment":                   resourceSegment(),
+			"launchdarkly_team_member":               resourceTeamMember(),
+			"launchdarkly_feature_flag_environment":  resourceFeatureFlagEnvironment(),
+			"launchdarkly_destination":               resourceDestination(),
+			"launchdarkly_access_token":              resourceAccessToken(),
+			"launchdarkly_flag_trigger":              resourceFlagTrigger(),
+			"launchdarkly_audit_log_subscription":    resourceAuditLogSubscription(),
+			"launchdarkly_relay_proxy_configuration": resourceRelayProxyConfig(),
+			"launchdarkly_metric":                    resourceMetric(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"launchdarkly_team":                      dataSourceTeam(),
+			"launchdarkly_team_member":               dataSourceTeamMember(),
+			"launchdarkly_team_members":              dataSourceTeamMembers(),
+			"launchdarkly_project":                   dataSourceProject(),
+			"launchdarkly_environment":               dataSourceEnvironment(),
+			"launchdarkly_feature_flag":              dataSourceFeatureFlag(),
+			"launchdarkly_feature_flag_environment":  dataSourceFeatureFlagEnvironment(),
+			"launchdarkly_webhook":                   dataSourceWebhook(),
+			"launchdarkly_segment":                   dataSourceSegment(),
+			"launchdarkly_flag_trigger":              dataSourceFlagTrigger(),
+			"launchdarkly_audit_log_subscription":    dataSourceAuditLogSubscription(),
+			"launchdarkly_relay_proxy_configuration": dataSourceRelayProxyConfig(),
+			"launchdarkly_metric":                    dataSourceMetric(),
+		},
+		ConfigureContextFunc: providerConfigure,
+	}
 }
 
-func (p *provider) Configure(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
-	var config providerData
-	diags := req.Config.Get(ctx, &config)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	var sdkKey string
-	if config.SDKKey.Unknown || config.SDKKey.Null {
-		sdkKey = os.Getenv(LAUNCHDARKLY_SDK_KEY)
-	} else {
-		sdkKey = config.SDKKey.Value
-	}
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	var diags diag.Diagnostics
 
-	if sdkKey == "" {
-		// Error vs warning - empty value must stop execution
-		resp.Diagnostics.AddError(
-			"SDK key not found",
-			"SDK Key must be provided",
-		)
-		return
+	// check environment variables first
+	accessToken := os.Getenv(LAUNCHDARKLY_ACCESS_TOKEN)
+	oauthToken := os.Getenv(LAUNCHDARKLY_OAUTH_TOKEN)
+	host := os.Getenv(LAUNCHDARKLY_API_HOST)
+
+	if host == "" {
+		host = DEFAULT_LAUNCHDARKLY_HOST
+	}
+	configHost := d.Get(API_HOST).(string)
+	if configHost != "" {
+		host = configHost
 	}
 
-	var ldConfig ld.Config
-	// TODO is there a shutdown call?
-	ldConfig.Events = ldcomponents.SendEvents()
-	// default poll interval is 30 seconds
-	ldConfig.DataSource = ldcomponents.PollingDataSource()
-
-	if config.Host.Value != "" {
-		ldConfig.ServiceEndpoints.Polling = config.Host.Value
+	if strings.HasPrefix(host, "http") {
+		u, _ := url.Parse(host)
+		host = u.Host
 	}
 
-	c, err := ld.MakeCustomClient(sdkKey, ldConfig, 5*time.Second)
+	// Check configuration data, which should take precedence over
+	// environment variable data, if found.
+	configAccessToken := d.Get(ACCESS_TOKEN).(string)
+	if configAccessToken != "" {
+		accessToken = configAccessToken
+	}
+	configOAuthToken := d.Get(OAUTH_TOKEN).(string)
+	if configOAuthToken != "" {
+		oauthToken = configOAuthToken
+	}
+	if oauthToken == "" && accessToken == "" {
+		return nil, diag.Errorf("either an %q or %q must be specified", ACCESS_TOKEN, OAUTH_TOKEN)
+	}
+
+	httpTimeoutSeconds := d.Get(HTTP_TIMEOUT).(int)
+	if httpTimeoutSeconds == 0 {
+		httpTimeoutSeconds = DEFAULT_HTTP_TIMEOUT_S
+	}
+
+	if oauthToken != "" {
+		client, err := newClient(oauthToken, host, true, httpTimeoutSeconds)
+		if err != nil {
+			return client, diag.FromErr(err)
+		}
+		return client, diags
+	}
+
+	client, err := newClient(accessToken, host, false, httpTimeoutSeconds)
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Unable to create client",
-			"Unable to initialize LaunchDarkly SDK client:\n\n"+err.Error(),
-		)
-		return
+		return client, diag.FromErr(err)
 	}
-
-	p.client = c
-	p.configured = true
-}
-
-func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
-	return map[string]tfsdk.ResourceType{}, nil
-}
-
-func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
-	return map[string]tfsdk.DataSourceType{
-		"ldflags_evaluation_boolean": dataSourceFlagEvaluationBooleanType{},
-		"ldflags_evaluation_string":  dataSourceFlagEvaluationStringType{},
-		"ldflags_evaluation_int":     dataSourceFlagEvaluationIntType{},
-		"ldflags_evaluation_float":   dataSourceFlagEvaluationFloatType{},
-		"ldflags_evaluation_json":    dataSourceFlagEvaluationJSONType{},
-	}, nil
+	return client, diags
 }
--- main.go
+++ main.go
@@ -2,13 +2,59 @@ package main
 
 import (
 	"context"
+	"flag"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	launchdarkly_flag_eval "github.com/angelolapus/terraform-provider-launchdarkly/launchdarkly"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
+	"github.com/angelolapus/terraform-provider-launchdarkly/launchdarkly"
 )
 
+// Run "go generate" to generate the docs
+
+// Install tools as needed
+//go:generate go install github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+//go:generate go install github.com/ashanbrown/gofmts/cmd/gofmts
+
+// Format examples
+//go:generate terraform fmt -recursive ./examples/
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --rendered-provider-name LaunchDarkly --provider-name launchdarkly
+
+// The version string gets updated at build time using -ldflags
+var version string = "development"
+
 func main() {
-	providerserver.Serve(context.Background(), launchdarkly_flag_eval.New, providerserver.ServeOpts{
-		Address: "registry.terraform.io/angelolapus/launchdarkly",
-	})
+	debugFlag := flag.Bool("debug", false, "Start provider in debug mode.")
+	flag.Parse()
+
+	ctx := context.Background()
+	providers := []func() tfprotov5.ProviderServer{
+		launchdarkly.Provider().GRPCProvider,
+
+		providerserver.NewProtocol5(
+			launchdarkly.NewPluginProvider(version)(),
+		),
+	}
+
+	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf5server.ServeOpt
+	if *debugFlag {
+		serveOpts = append(serveOpts, tf5server.WithManagedDebug())
+	}
+
+	err = tf5server.Serve(
+		"registry.terraform.io/angelolapus/launchdarkly",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
